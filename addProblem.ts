821b600f861b987328e34eb21c9f36b062daba86
Kamran Ansari <ranmerc@outlook.com>
2025-10-28 23:35:47 +0530

    feat: derive problem name from problem url
    Also clean up URL automatically

--- addProblem.ts
+++ addProblem.ts
@@ -3,8 +3,18 @@ import type { ProblemsDB } from "./db/db";
 import { readUserInput } from "./utils/input";
 
 export const handleAddProblem = async (db: ProblemsDB) => {
-  const name = await readUserInput("Problem name: ");
-  const url = await readUserInput("Problem URL: ");
+  const inputURL = await readUserInput("Problem URL: ");
+
+  // eg. https://leetcode.com/problems/set-matrix-zeroes/description/ ->
+  // https://leetcode.com/problems/set-matrix-zeroes
+  const url = inputURL.replace(
+    /(https\:\/\/leetcode.com\/problems\/.*)\/.+/,
+    "$1"
+  );
+
+  // eg. set-matrix-zeroes -> SET MATRIX ZEROES
+  const name = url.split("/").pop()?.replaceAll("-", " ").toUpperCase();
+
   const confidence = Number(await readUserInput("Confidence level: "));
   // Convert the date to a string in the format "YYYY-MM-DD", stripping the time
   const date = new Date().toISOString().substring(0, 10);
