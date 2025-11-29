d1985c9681a39441552b41d57e96a3ba1a6a3492
Kamran Ansari <ranmerc@outlook.com>
2025-11-29 23:59:40 +0530

    refactor: explicitly mention column names in query

--- db/sqlQueries.ts
+++ db/sqlQueries.ts
@@ -1,8 +1,8 @@
-export const getProblemsQuery = `SELECT * FROM 'problems'`;
+export const getProblemsQuery = `SELECT name, url, date, confidence, times FROM 'problems'`;
 
 export const addProblemQuery = `
-  INSERT INTO 'problems' (name, url, date, confidence, times) 
+  INSERT INTO 'problems' (name, url, date, confidence, times)
   VALUES ($1, $2, $3, $4, $5)
-  ON CONFLICT(url) 
+  ON CONFLICT(url)
   DO UPDATE SET times = problems.times + 1, date = $3, confidence = $4;
 `;
